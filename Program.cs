9be6ca19e65ec29c0d9e2de19d9bc6de28a87174
TarynL <tarlytle@gmail.com>
2021-05-25 15:33:14 -0500

    Phase 7 complete

--- Program.cs
+++ Program.cs
@@ -4,42 +4,74 @@
 
 question();
 
+
 void question()
 {
-    Console.WriteLine("What is the secret number?");
-    int guess = int.Parse(Console.ReadLine());
-    Random r = new Random();
-    int secretNum = r.Next(0, 100);
-    int guessLimit = 4;
+    int secretNum = new Random().Next(0, 100);
+    int guessLimit = 0;
+    int difficultyLevel = 0;
+    int easy = 1;
+    int medium = 2;
+    int hard = 3;
 
-    for (int i = 1; i < 4; i++)
+
+    while (difficultyLevel == 0)
+    {
+        Console.WriteLine("What difficulty level would you like?");
+        Console.WriteLine($"{easy} {medium} {hard}");
+
+        difficultyLevel = int.Parse(Console.ReadLine());
+        if (difficultyLevel == easy)
+        {
+            guessLimit = 8;
+
+        }
+        else if (difficultyLevel == medium)
+        {
+            guessLimit = 6;
+
+        }
+        else if (difficultyLevel == hard)
+        {
+            guessLimit = 4;
+
+        }
+        else
+        {
+            Console.WriteLine("Please enter a valid selection.");
+        }
+
+
+    }
+
+    for (int i = 1; i < guessLimit; i++)
     {
 
+        Console.WriteLine("What is the secret number?");
+        int guess = int.Parse(Console.ReadLine());
 
         if (guess == secretNum)
         {
             Console.WriteLine("You guessed it!!!");
-            guess = int.Parse(Console.ReadLine());
+
         }
 
         else if (guess < secretNum)
         {
             Console.WriteLine($"Your guess was too low. You have {guessLimit - i} guesses left. Try again!");
-            guess = int.Parse(Console.ReadLine());
+
         }
 
         else if (guess > secretNum)
         {
             Console.WriteLine($"Your guess was too high. You have {guessLimit - i} guesses left. Try again!");
-            guess = int.Parse(Console.ReadLine());
+
         }
-
-
-
-
 
     }
 }
+
+
 
 
 
