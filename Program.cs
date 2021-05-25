25937fc0f07e4f766dedda4284370348ae1861e0
TarynL <tarlytle@gmail.com>
2021-05-25 14:36:12 -0500

    Phase 5 complete

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-int secretNum = 42;
+
 
 question();
 
@@ -8,12 +8,15 @@ void question()
 {
     Console.WriteLine("What is the secret number?");
     int guess = int.Parse(Console.ReadLine());
+    Random r = new Random();
+    int secretNum = r.Next(0, 100);
+    int guessLimit = 4;
 
     for (int i = 1; i < 4; i++)
     {
         if (guess != secretNum)
         {
-            Console.WriteLine($"Guess #{i} Try Again");
+            Console.WriteLine($"You have {guessLimit - i} guesses left. Try Again");
             guess = int.Parse(Console.ReadLine());
         }
 
