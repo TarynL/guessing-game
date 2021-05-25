30b189ec13139b8df70207ba93cffe7146dd6f46
TarynL <tarlytle@gmail.com>
2021-05-25 14:47:25 -0500

    Phase 6 complete

--- Program.cs
+++ Program.cs
@@ -14,17 +14,28 @@ void question()
 
     for (int i = 1; i < 4; i++)
     {
-        if (guess != secretNum)
+
+
+        if (guess == secretNum)
         {
-            Console.WriteLine($"You have {guessLimit - i} guesses left. Try Again");
+            Console.WriteLine("You guessed it!!!");
             guess = int.Parse(Console.ReadLine());
         }
 
-        else
+        else if (guess < secretNum)
         {
-            Console.WriteLine("You guessed it!!!");
-
+            Console.WriteLine($"Your guess was too low. You have {guessLimit - i} guesses left. Try again!");
+            guess = int.Parse(Console.ReadLine());
         }
+
+        else if (guess > secretNum)
+        {
+            Console.WriteLine($"Your guess was too high. You have {guessLimit - i} guesses left. Try again!");
+            guess = int.Parse(Console.ReadLine());
+        }
+
+
+
 
 
     }
